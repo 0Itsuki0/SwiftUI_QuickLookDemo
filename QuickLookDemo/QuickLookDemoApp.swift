2166c52d62293410a4587a46c183ce07913c91a4
itsuki <itsuki.enjyoy@gmail.com>
2025-09-28 14:48:09 +0900

    Initial Commit

---
+++ QuickLookDemo/QuickLookDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickLookDemoApp.swift
+//  QuickLookDemo
+//
+//  Created by Itsuki on 2025/09/28.
+//
+
+import SwiftUI
+
+@main
+struct QuickLookDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
